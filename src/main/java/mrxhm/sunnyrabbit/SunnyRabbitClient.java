7ff0f3f09000d08bb3a28ec0a3ef187cffa173c6
Mr_XHM <13022056+mr-xhm@user.noreply.gitee.com>
2024-03-10 11:32:06 +0800

    Initial commit

---
+++ src/main/java/mrxhm/sunnyrabbit/SunnyRabbitClient.java
@@ -0,0 +1,10 @@
+package mrxhm.sunnyrabbit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SunnyRabbitClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/mrxhm/sunnyrabbit/SunnyRabbitDataGenerator.java
@@ -0,0 +1,11 @@
+package mrxhm.sunnyrabbit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SunnyRabbitDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
